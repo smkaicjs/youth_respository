95dc25fa652edea587cedd3914e8b3309e172840
smkaicjs <shimingkun@protonmail.com>
2021-04-14 18:28:12 +0800

    【what】百度地图获取当前位置

--- app/src/main/java/com/shimk/Txgc/frag/MapFragment.java
+++ app/src/main/java/com/shimk/Txgc/frag/MapFragment.java
@@ -2,6 +2,7 @@ package com.shimk.Txgc.frag;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -11,10 +12,22 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.LogoPosition;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.shimk.Txgc.R;
 import com.shimk.Txgc.activity.ContentActivity;
 import com.shimk.Txgc.utils.LogOkhttp;
+import com.shimk.Txgc.utils.ShimkLog;
 
 
 import io.reactivex.Observable;
@@ -30,6 +43,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 import retrofit2.http.GET;
 
+import static com.shimk.Txgc.utils.ShimkLog.TAG;
 import static com.shimk.Txgc.utils.ShimkLog.logd;
 
 /**
@@ -41,8 +55,13 @@ public class MapFragment extends Fragment {
 
     private TextView textView;
 
-    private MapView baiduMap;
+    private MapView baiduMapView;
+    private BaiduMap map;
     private ContentActivity mActivity;
+    private LocationClient mLocationClient;
+    private FloatingActionButton floatingActionButtonChangeMapType;
+
+    public static int maptype = 1;
 
 
     public MapFragment() {
@@ -60,9 +79,21 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        mActivity = (ContentActivity) getActivity();
         View view = inflater.inflate(R.layout.fragment_baidumap, container, false);
         textView = view.findViewById(R.id.rejava_text);
-        baiduMap = view.findViewById(R.id.baidu_mao_view);
+        baiduMapView = view.findViewById(R.id.baidu_mao_view);
+        floatingActionButtonChangeMapType = view.findViewById(R.id.change_map_type);
+
+        map = baiduMapView.getMap();
+        baiduMapView.setLogoPosition(LogoPosition.logoPostionRightBottom);
+
+
+
+        initData();
+
+        //显示交通情况
+
 
         
         textView.setOnClickListener(new View.OnClickListener() {
@@ -73,10 +104,69 @@ public class MapFragment extends Fragment {
                 textRxjava.testModel();
             }
         });
-        mActivity = (ContentActivity) getActivity();
+        floatingActionButtonChangeMapType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (maptype==1){
+                    map.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+                    maptype = 2;
+
+                }else if (maptype==2){
+                    map.setMapType(BaiduMap.MAP_TYPE_NONE);
+                    maptype = 3;
+                }else{
+                    map.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+                    maptype = 1;
+                }
+            }
+        });
+
         return view;
     }
 
+    private void initData() {
+
+        map.setTrafficEnabled(true);
+        //开启定位
+        map.setMyLocationEnabled(true);
+
+        //开启指南针
+        map.setOnMapLoadedCallback(new BaiduMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                ShimkLog.logd("地图加载完成");
+                map.setCompassEnable(true);
+                map.setViewPadding(1000,10,0,50);
+
+            }
+        });
+
+
+        mLocationClient = new LocationClient(this.getContext());
+
+//通过LocationClientOption设置LocationClient相关参数
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(500);
+
+
+//设置locationClientOption
+        mLocationClient.setLocOption(option);
+
+//注册LocationListener监听器
+        BaiduLocationData myLocationListener = new BaiduLocationData();
+        mLocationClient.registerLocationListener(myLocationListener);
+//开启地图定位图层
+        mLocationClient.start();
+        View view = baiduMapView.getChildAt(2);
+        baiduMapView.removeView(view);
+
+
+
+
+    }
 
 
     private View.OnTouchListener baiduOnTouchListener = new View.OnTouchListener() {
@@ -90,15 +180,57 @@ public class MapFragment extends Fragment {
             return false;
         }
     };
+
+    @Override
+    public void onPause() {
+        baiduMapView.onPause();
+        super.onPause();
+
+    }
+
     @Override
     public void onResume() {
+        baiduMapView.onResume();
         super.onResume();
-        baiduMap.onResume();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mLocationClient.stop();
+        map.setMyLocationEnabled(false);
+        baiduMapView.onDestroy();
+        baiduMapView = null;
     }
 
     public static <T> ObservableTransformer<T, T> observableToMain() {
         return upstream -> upstream.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    class BaiduLocationData extends BDAbstractLocationListener{
+        @Override
+        public void onReceiveLocation(BDLocation bdLocation) {
+
+            if (bdLocation == null || baiduMapView == null){
+                Log.d(TAG, "onReceiveLocation: 空地");
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(bdLocation.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(bdLocation.getDirection()).latitude(bdLocation.getLatitude())
+                    .longitude(bdLocation.getLongitude()).build();
+            map.setMyLocationData(locData);
+
+            //自定义配置位置图标
+            ShimkLog.logd(baiduMapView.getChildCount()+"个子视图");
+            map.setMyLocationConfiguration(new MyLocationConfiguration(MyLocationConfiguration.LocationMode.COMPASS,true
+                    ,BitmapDescriptorFactory.fromResource(R.drawable.icon_index),
+                    0xfEEfffff,
+                    0x9fffffff));
+
+        }
     }
 
     class textRxjava{
